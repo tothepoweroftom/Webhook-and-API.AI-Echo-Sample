efe8aa56798668a8b615442b2e054b11df3485a7
Tom Power <tom.power@mobgen.com>
2017-04-19 14:49:17 +0200

    asd

--- index.js
+++ index.js
@@ -1,126 +1,29 @@
 'use strict';
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const randWord = require('random-words');
+const ApiAiAssistant = require('actions-on-google').ApiAiAssistant;
 
-const restService = express();
+exports.themostinterestingman = (req, res) => {
+  const assistant = new ApiAiAssistant({request: request, response: response});
 
-String.prototype.shuffle = function() {
-    return this.split(" ").map(function(word, i) {
-        var a = word.split(""),
-            n = a.length;
+  const WELCOME_INTENT = 'custom.welcome';  // the action name from the API.AI intent
 
-        for (var i = n - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var tmp = a[i];
-            a[i] = a[j];
-            a[j] = tmp;
-        }
-        return a.join("");
-    }).join(" ");
+  let actionMap = new Map();
+  actionMap.set(WELCOME_INTENT, welcomeIntent);
+  // Create functions to handle requests here
+
+
+  function saySSML(assistant) {
+  let text_to_speech = '<speak>'
+    + 'Here are <say-as interpret-as="characters">SSML</say-as> samples. '
+    + 'I can pause <break time="3"/>. '
+    + 'I can play a sound <audio src="https://www.example.com/MY_WAVE_FILE.wav">your wave file</audio>. '
+    + 'I can speak in cardinals. Your position is <say-as interpret-as="cardinal">10</say-as> in line. '
+    + 'Or I can speak in ordinals. You are <say-as interpret-as="ordinal">10</say-as> in line. '
+    + 'Or I can even speak in digits. Your position in line is <say-as interpret-as="digits">10</say-as>. '
+    + 'I can also substitute phrases, like the <sub alias="World Wide Web Consortium">W3C</sub>. '
+    + 'Finally, I can speak a paragraph with two sentences. '
+    + '<p><s>This is sentence one.</s><s>This is sentence two.</s></p>'
+    + '</speak>'
+  assistant.tell(text_to_speech);
+};
 }
-
-//Random words
-String.prototype.randomWord = function() {
-    var vowels = ['a', 'e', 'i', 'o', 'u'];
-    var consts = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'qu', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'tt', 'ch', 'sh'];
-
-    var len = 4;
-
-    var word = '';
-    var word2 = '';
-    var word3 = '';
-
-
-    var is_vowel = false;
-
-    var arr;
-
-    for (var i = 0; i < len; i++) {
-
-        if (is_vowel) arr = vowels
-        else arr = consts
-        is_vowel = !is_vowel;
-
-        word += arr[Math.round(Math.random() * (arr.length - 1))];
-    }
-    return word;
-}
-
-restService.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-restService.use(bodyParser.json());
-
-restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    var sentence = words({min: 8, max: 12}).join(" "); // => "which least vegetable wool poem wife golden"
-    return res.json({
-        speech: sentence,
-        displayText: sentence,
-        source: 'webhook-echo-sample'
-    });
-});
-
-restService.post('/slack-test', function(req, res) {
-
-    var slack_message = {
-        "text": "Details of JIRA board for Browse and Commerce",
-        "attachments": [{
-            "title": "JIRA Board",
-            "title_link": "http://www.google.com",
-            "color": "#36a64f",
-
-            "fields": [{
-                "title": "Epic Count",
-                "value": "50",
-                "short": "false"
-            }, {
-                "title": "Story Count",
-                "value": "40",
-                "short": "false"
-            }],
-
-            "thumb_url": "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
-        }, {
-            "title": "Story status count",
-            "title_link": "http://www.google.com",
-            "color": "#f49e42",
-
-            "fields": [{
-                "title": "Not started",
-                "value": "50",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }]
-        }]
-    }
-    return res.json({
-        speech: "speech",
-        displayText: "speech",
-        source: 'webhook-echo-sample',
-        data: {
-            "slack": slack_message
-        }
-    });
-});
-
-
-
-
-restService.listen((process.env.PORT || 8000), function() {
-    console.log("Server up and listening");
-});
